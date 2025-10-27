3e3e3182816ee0b2853d06f7b03c3ef3b18c09e9
andrewssteven <andrewssteven151@gmail.com>
2025-10-27 22:05:13 +0100

    fix:autoload fix2

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
 $twig = new \Twig\Environment($loader, [
--- public/vendor/composer/installed.php
+++ public/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'app/ticket-management-twig',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2601afeb856ef9bdbea7f92243f639db70f6a82e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'app/ticket-management-twig' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2601afeb856ef9bdbea7f92243f639db70f6a82e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
