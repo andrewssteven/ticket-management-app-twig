2601afeb856ef9bdbea7f92243f639db70f6a82e
andrewssteven <andrewssteven151@gmail.com>
2025-10-27 21:41:29 +0100

    fix:autoload fix

--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php
-require_once __DIR__ . '../vendor/autoload.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/../templates');
 $twig = new \Twig\Environment($loader, [
